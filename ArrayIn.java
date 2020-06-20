4d51317134016ce6a0d32b1af6608ee60b48e678
NAVNEET SAHAY <56154963+NavneetSahay@users.noreply.github.com>
2020-06-20 11:46:37 +0530

    Add files via upload

---
+++ ArrayIn.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class ArrayIn
+{
+	public static void main(String[] args) {
+		int[] students = new int[10];
+	 for (int i=0;i<5 ;i++ )
+	 System.out.println("NUMBER OF STUDENT :"+students[i]);
+	}
+}
