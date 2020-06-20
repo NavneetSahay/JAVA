1b77dacbf32a658eb8b6e81e892883bb9c0dc722
NAVNEET SAHAY <56154963+NavneetSahay@users.noreply.github.com>
2020-06-20 11:57:53 +0530

    Add files via upload

---
+++ ArrayIndex.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class ArrayIndex
+{
+	public static void main(String[] args) {
+		int[] element = {187,63,65,87,64,98,646};
+	 for (int i=0;i<5 ;i++ )
+	 System.out.println("NUMBER stored in  " +i+ "th index:"+element[i]);
+	}
+}
