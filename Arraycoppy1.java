49296a644346945537fab4912593424690f62d8e
NAVNEET SAHAY <56154963+NavneetSahay@users.noreply.github.com>
2020-06-22 16:22:37 +0530

    Add files via upload

---
+++ Arraycoppy1.java
@@ -0,0 +1,10 @@
+public class Arraycoppy1
+{
+	public static void main(String[] args)
+	{
+	    int a []= {73,5,23,4,21,12,34};
+	    int b []= a; //coping an array y
+	    for (int i=0;i<=a.length-1;i++) 
+		System.out.print(a[i] +",");
+	}
+}
