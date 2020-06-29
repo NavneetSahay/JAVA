a8c0700b31a2a96db53884fe712788a681eac2e6
NAVNEET SAHAY <navneetkumarsahay@gmail.com>
2020-06-29 10:00:06 +0530

    Add files via upload

---
+++ Stringcomp.java
@@ -0,0 +1,11 @@
+class Stringcomp{
+  public static void main(String[] args) {
+    String fname = "GOOGLE";
+    String sname = "GOOGLA";
+    String tname = "GOOGLE";
+    boolean result1 = fname.equals(sname);
+    boolean reslut2 = fname.equals(tname);
+    System.out.println("String 1st nd 2nd are equal :"+result1);
+    System.out.println("String 1st nd 3rd are equal :"+reslut2);
+  }
+}
